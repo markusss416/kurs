209149aa1a1a4b4158d74677cdbb4678211a9c77
markusss416 <128100304+markusss416@users.noreply.github.com>
2023-05-29 14:55:45 +0300

    Update base.sql

--- base.sql
+++ base.sql
@@ -1,32 +1,44 @@
-CREATE TABLE `avia`.`flight` (
-  `id` INT NOT NULL,
-  `type_id` VARCHAR(45) NULL,
-  `user_id` VARCHAR(45) NULL,
-  `date_flight` VARCHAR(45) NULL,
-  `status` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`));
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Час створення: Трв 26 2023 р., 20:51
+-- Версія сервера: 8.0.24
+-- Версія PHP: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-CREATE TABLE `avia`.`flight_type` (
-  `id` INT NOT NULL,
-  `name` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`));
-  
-  CREATE TABLE `avia`.`users` (
-  `id` INT NOT NULL,
-  `login` VARCHAR(45) NULL,
-  `name` VARCHAR(45) NULL,
-  `surname` VARCHAR(45) NULL,
-  `phone` VARCHAR(45) NULL,
-  `password` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`));
-  
-ALTER TABLE flight
-ADD FOREIGN KEY (type_id) References flight_type (ID)
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-ALTER Table flight
-ADD FOREIGN KEY (user_id) 
-REFERENCES users(id)
+--CREATE DATABASE IF NOT EXISTS `avia1` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `avia1`;
+-- База даних: `avia1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблиці `flight`
+--
+
+CREATE TABLE `flight` (
+  `id` int NOT NULL,
+  `type_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `date_flight` date NOT NULL,
+  `status` int NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Дамп даних таблиці `flight`
+--
 
 INSERT INTO `flight` (`id`, `type_id`, `user_id`, `date_flight`, `status`) VALUES
 (4, 2, 2, '2023-05-12', 1),
@@ -37,6 +49,20 @@ INSERT INTO `flight` (`id`, `type_id`, `user_id`, `date_flight`, `status`) VALUE
 (9, 12, 5, '2023-06-08', 0),
 (10, 7, 5, '2023-06-07', 1);
 
+-- --------------------------------------------------------
+
+--
+-- Структура таблиці `flight_type`
+--
+
+CREATE TABLE `flight_type` (
+  `id` int NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Дамп даних таблиці `flight_type`
+--
 
 INSERT INTO `flight_type` (`id`, `name`) VALUES
 (2, 'Львів - Одесса'),
@@ -49,6 +75,24 @@ INSERT INTO `flight_type` (`id`, `name`) VALUES
 (13, 'Львів - Запоріжжя'),
 (14, 'Харків - Запоріжжя');
 
+-- --------------------------------------------------------
+
+--
+-- Структура таблиці `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `login` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `surname` varchar(50) NOT NULL,
+  `phone` varchar(15) NOT NULL,
+  `password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Дамп даних таблиці `users`
+--
 
 INSERT INTO `users` (`id`, `login`, `name`, `surname`, `phone`, `password`) VALUES
 (1, 'admin', 'admin', 'admin', 'none', '1234'),
@@ -56,3 +100,52 @@ INSERT INTO `users` (`id`, `login`, `name`, `surname`, `phone`, `password`) VALU
 (3, 'vasyl', 'Василь', 'Бондаренко', '+380953232655', '1234'),
 (4, 'olena', 'Olena', 'Vakula', '+380985252568', '1234'),
 (5, 'maria', 'Марія', 'Гончаренко', '+380505478354', '1234');
+
+--
+-- Індекси збережених таблиць
+--
+
+--
+-- Індекси таблиці `flight`
+--
+ALTER TABLE `flight`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Індекси таблиці `flight_type`
+--
+ALTER TABLE `flight_type`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Індекси таблиці `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для збережених таблиць
+--
+
+--
+-- AUTO_INCREMENT для таблиці `flight`
+--
+ALTER TABLE `flight`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT для таблиці `flight_type`
+--
+ALTER TABLE `flight_type`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT для таблиці `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
